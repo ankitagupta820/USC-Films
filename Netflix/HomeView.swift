d77af3d2d90baec0559a9688252e1f4d04402ab4
Ankita Gupta <ankita.er.gupta@gmail.com>
2021-03-18 16:35:15 -0700

    Adding HomeScreen

--- Netflix/HomeView.swift
+++ Netflix/HomeView.swift
@@ -6,30 +6,187 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct HomeView: View {
+    
     var body: some View {
         
-            NavigationView{
-                VStack(){
-                    Text("Home View")
-                    Spacer()
-                    HStack(){
-                        
-                        NavigationLink(destination: DetailsView(movieID:"Making Their Mark", videoURL:"https://youtu.be/8jVuOheTNGQ", DetailsVM: DetailVM(ticker: "Making Their Mark"))){
-                            Text("Detail")
+        NavigationView{
+            ScrollView{
+                
+                // Now Playing
+                VStack(alignment: .leading){
+                    Text("Now Playing").font(.system(size: 25.0, design:.rounded)).fontWeight(.bold)
+                    ScrollView(.horizontal){
+                        HStack(spacing: 30){
+                            
+                            ForEach(0..<20){num in
+                                
+                                NavigationLink(destination: DetailsView(movieID:"Making Their Mark", videoURL:"https://youtu.be/8jVuOheTNGQ", DetailsVM: DetailVM(ticker: "Making Their Mark"))){
+                                    
+                                    VStack(){
+                                        KFImage(URL(string: "https://www.themoviedb.org/t/p/w276_and_h350_face/485V2gC6w1O9D96KUtKPyJpgm2j.jpg"))
+                                            .resizable()
+                                            .scaledToFill()
+                                            .cornerRadius(5)
+                                            .frame(width:150, height:200)
+                                        
+                                        Text("Movie Name").font(.caption).fixedSize(horizontal: false, vertical: true)
+                                        Text("("+"2020"+")").font(.caption)
+                                    }.frame(width: 150)
+                                    
+                                }.background(Color.white)
+                                .contentShape(RoundedRectangle(cornerRadius: 10))
+                                .contextMenu(menuItems: {
+                                    let source = "https://www.themoviedb.org/t/p/w276_and_h350_face/485V2gC6w1O9D96KUtKPyJpgm2j.jpg"
+                                    
+                                    //Twitter
+                                    let TwitterShareString = String("https://twitter.com/intent/tweet?text=Check out this link: &url=\(source)&hashtags=CSCI571StockApp")
+                                    let escapedShareString = TwitterShareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+                                    let twitterUrl: URL = URL(string: escapedShareString)!
+                                    
+                                    //Facebook
+                                    let FacebookShareString = String("https://www.facebook.com/sharer/sharer.php?u="+source)
+                                    let escapedFacebook = FacebookShareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+                                    let fbUrl: URL = URL(string:escapedFacebook)!
+                                    
+                                    
+                                    //Youtube Link
+                                    let YoutubeShareUrl: URL = URL(string: source)!
+                                    
+                                    
+                                    Button {
+                                        print("Change country setting")
+                                    } label: {
+                                        Label("Add to watchList", systemImage:"bookmark.fill")
+                                    }
+                                    
+                                    Link(destination: YoutubeShareUrl, label: {Label("Watch Trailer", systemImage: "film")})
+                                    Link(destination: fbUrl, label: {Label("Share on Facebook", image: "Facebook")})
+                                    Link(destination: twitterUrl, label: {Label("Share on Twitter", image: "Twitter")})
+                                    
+                                })
+                            }
                         }
+                    }
+                }
+                
+                
+                //Top Rated
+                VStack(alignment: .leading){
+                    Text("Top Rated").font(.system(size: 25.0, design:.rounded)).fontWeight(.bold)
+                    ScrollView(.horizontal){
+                        HStack(spacing: 30){
+                            
+                            ForEach(0..<20){num in
+                                
+                                NavigationLink(destination: DetailsView(movieID:"Making Their Mark", videoURL:"https://youtu.be/8jVuOheTNGQ", DetailsVM: DetailVM(ticker: "Making Their Mark"))){
+                                    
+                                    
+                                    VStack(){
+                                        KFImage(URL(string: "https://www.themoviedb.org/t/p/w276_and_h350_face/485V2gC6w1O9D96KUtKPyJpgm2j.jpg"))
+                                            .resizable()
+                                            .scaledToFill()
+                                            .cornerRadius(5)
+                                            .frame(height: 150)
+                                        Text("Movie Name").font(.caption).fixedSize(horizontal: false, vertical: true)
+                                        Text("("+"2020"+")").font(.caption)
+                                    }.frame(width: 100)
                         
-                        NavigationLink(destination: WatchlistView()){
-                            Text("Watchlist")
+                                }.background(Color.white)
+                                .contentShape(RoundedRectangle(cornerRadius: 10))
+                                .contextMenu(menuItems: {
+                                    let source = "https://www.themoviedb.org/t/p/w276_and_h350_face/485V2gC6w1O9D96KUtKPyJpgm2j.jpg"
+                                    
+                                    //Twitter
+                                    let TwitterShareString = String("https://twitter.com/intent/tweet?text=Check out this link: &url=\(source)&hashtags=CSCI571StockApp")
+                                    let escapedShareString = TwitterShareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+                                    let twitterUrl: URL = URL(string: escapedShareString)!
+                                    
+                                    //Facebook
+                                    let FacebookShareString = String("https://www.facebook.com/sharer/sharer.php?u="+source)
+                                    let escapedFacebook = FacebookShareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+                                    let fbUrl: URL = URL(string:escapedFacebook)!
+                                    
+                                    //Youtube Link
+                                    let YoutubeShareUrl: URL = URL(string: source)!
+                                    
+                                    
+                                    Button {
+                                        print("Change country setting")
+                                    } label: {
+                                        Label("Add to watchList", systemImage:"bookmark.fill")
+                                    }
+                                    
+                                    Link(destination: YoutubeShareUrl, label: {Label("Watch Trailer", systemImage: "film")})
+                                    Link(destination: fbUrl, label: {Label("Share on Facebook", image: "Facebook")})
+                                    Link(destination: twitterUrl, label: {Label("Share on Twitter", image: "Twitter")})
+                                    
+                                })
+                            }
                         }
-                        
-                        NavigationLink(destination: SearchView()){
-                            Text("Search")
+                    }
+                }
+                
+                //Popular
+                VStack(alignment: .leading){
+                    Text("Popular").font(.system(size: 25.0, design:.rounded)).fontWeight(.bold)
+                    ScrollView(.horizontal){
+                        HStack(spacing: 30){
+                            
+                            ForEach(0..<20){num in
+                                
+                                NavigationLink(destination: DetailsView(movieID:"Making Their Mark", videoURL:"https://youtu.be/8jVuOheTNGQ", DetailsVM: DetailVM(ticker: "Making Their Mark"))){
+                                    
+                                    VStack(){
+                                        KFImage(URL(string: "https://www.themoviedb.org/t/p/w276_and_h350_face/485V2gC6w1O9D96KUtKPyJpgm2j.jpg"))
+                                            .resizable()
+                                            .scaledToFill()
+                                            .cornerRadius(5)
+                                            .frame(height: 150)
+                                        Text("Movie Name").font(.caption).fixedSize(horizontal: false, vertical: true)
+                                        Text("("+"2020"+")").font(.caption)
+                                    }.frame(width: 100)
+                                    
+                                }.background(Color.white)
+                                .contentShape(RoundedRectangle(cornerRadius: 10))
+                                .contextMenu(menuItems: {
+                                    let source = "https://www.themoviedb.org/t/p/w276_and_h350_face/485V2gC6w1O9D96KUtKPyJpgm2j.jpg"
+                                    
+                                    //Twitter
+                                    let TwitterShareString = String("https://twitter.com/intent/tweet?text=Check out this link: &url=\(source)&hashtags=CSCI571StockApp")
+                                    let escapedShareString = TwitterShareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+                                    let twitterUrl: URL = URL(string: escapedShareString)!
+                                    
+                                    //Facebook
+                                    let FacebookShareString = String("https://www.facebook.com/sharer/sharer.php?u="+source)
+                                    let escapedFacebook = FacebookShareString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+                                    let fbUrl: URL = URL(string:escapedFacebook)!
+
+                                    //Youtube Link
+                                    let YoutubeShareUrl: URL = URL(string: source)!
+
+                                    Button {
+                                        print("Change country setting")
+                                    } label: {
+                                        Label("Add to watchList", systemImage:"bookmark.fill")
+                                    }
+                                    
+                                    Link(destination: YoutubeShareUrl, label: {Label("Watch Trailer", systemImage: "film")})
+                                    Link(destination: fbUrl, label: {Label("Share on Facebook", image: "Facebook")})
+                                    Link(destination: twitterUrl, label: {Label("Share on Twitter", image: "Twitter")})
+                                    
+                                })
+                            }
+                            
                         }
                     }
                 }
             }
+            .navigationBarTitle("Netflix")
+        }.padding()
+        
     }
 }
 
--- Netflix/NetflixApp.swift
+++ Netflix/NetflixApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct NetflixApp: App {
+    
+    @State var selection = 2
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            //HomeView()
+            TabView(selection:$selection){
+                SearchView()
+                    .tabItem{Label("Search", systemImage:"magnifyingglass")}
+                    .tag(1)
+                HomeView()
+                    .tabItem{Label("Home", systemImage:"house")}
+                    .tag(2)
+                WatchlistView()
+                    .tabItem{Label("WatchList", systemImage:"heart")}
+                    .tag(3)
+            }
         }
     }
 }
